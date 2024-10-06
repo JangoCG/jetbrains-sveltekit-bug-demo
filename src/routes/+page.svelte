93b6864307b746f3871b1e9adfd456c48652aa24
Cengiz Gürtusgil <cengiz.guertusgil@gmail.com>
2024-10-06 20:21:55 +0200

    bug report

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Card from '$lib/components/Card.svelte';
+</script>
+
+<!--Import of Card works, because it beginners with an upper case letter-->
+<Card></Card>
+
+<!--Import of Button will never work, because the file name starts with an lower case -->
+<Button></Button>
